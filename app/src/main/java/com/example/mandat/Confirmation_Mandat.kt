96b2fddf586c259fd9ed5b00f4a9e3714f33772e
amirmansouri <amir27069149@gmail.com>
2022-02-13 18:12:27 +0100

    mandat

--- app/src/main/java/com/example/mandat/Confirmation_Mandat.kt
+++ app/src/main/java/com/example/mandat/Confirmation_Mandat.kt
@@ -1,8 +1,12 @@
 package com.example.mandat
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import  com.example.mandat.WesternUnion
@@ -10,11 +14,13 @@ import java.util.*
 
 class Confirmation_Mandat : AppCompatActivity() {
 
+    @SuppressLint("NewApi")
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_confirmation_mandat)
-
+val layoutInflater : LayoutInflater = layoutInflater
+        val  viewLay = layoutInflater.inflate(R.layout.valid,findViewById(R.id.valid_))
         val expediteur = findViewById<TextView>(R.id.expediteur) as TextView
         val type = findViewById<TextView>(R.id.type) as TextView
         val codeOperation: TextView = findViewById<TextView>(R.id.codeOperation) as TextView
@@ -22,6 +28,7 @@ class Confirmation_Mandat : AppCompatActivity() {
         val date = findViewById<TextView>(R.id.date) as TextView
         val total = findViewById<TextView>(R.id.total) as TextView
         val button5 = findViewById<Button>(R.id.button5) as Button
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar) as ProgressBar
 val intent=intent
         val codeW=intent.getStringExtra("codeW")
         expediteur.text = codeW
@@ -33,7 +40,24 @@ val intent=intent
         val montant = intent.getStringExtra("montant")
         total.text = montant
       button5.setOnClickListener{
-          Toast.makeText(this,"validation terminer ",Toast.LENGTH_SHORT).show()
+         var bar = 0
+        Thread(Runnable {
+              while (bar <100){
+                  bar +=1
+                  try {
+                      Thread.sleep(20)
+                     progressBar.setProgress(bar)
+                  }
+                  catch (exp:InterruptedException){
+                      exp.printStackTrace()
+                  }
+                  }
+          }).start()
+
+val valid = Toast.makeText(this,"",Toast.LENGTH_SHORT)
+          valid.setGravity(Gravity.CENTER,0,0)
+          valid.view=viewLay
+          valid.show()
       }
 
     }
--- app/src/main/java/com/example/mandat/WesternUnion.kt
+++ app/src/main/java/com/example/mandat/WesternUnion.kt
@@ -1,21 +1,10 @@
 package com.example.mandat
 
-import android.annotation.SuppressLint
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.pdf.PdfDocument
-import android.os.Build
 import android.os.Bundle
-import android.os.Environment
 import android.widget.*
-import androidx.annotation.RequiresPermission
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import com.google.firebase.database.FirebaseDatabase
-import com.itextpdf.text.Document
-import com.itextpdf.text.Paragraph
-import com.itextpdf.text.pdf.PdfWriter
-import java.io.FileOutputStream
 import java.lang.Exception
 import java.text.SimpleDateFormat
 import java.util.*
@@ -23,6 +12,8 @@ import java.util.jar.Manifest
 
 
 class WesternUnion : AppCompatActivity() {
+
+     private   lateinit var  myIntent:Intent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,97 +23,84 @@ class WesternUnion : AppCompatActivity() {
         val codeW = findViewById<EditText>(R.id.codeW) as EditText
         val pinw = findViewById<EditText>(R.id.pinW) as EditText
         val montantw = findViewById<TextView>(R.id.montantW) as TextView
-        var codeOperateur : String = ""
-        var pays: String=""
-        var montant:String=""
+        var codeOperateur: String = ""
+        var pays: String = ""
+        var montant: String = ""
         val radioButton = findViewById<RadioButton>(R.id.radioButton) as RadioButton
 
 //read from Firebase
-        buttonW.setOnClickListener{
-        var database = FirebaseDatabase.getInstance().getReference("Users")
-        database.child("amir").get().addOnSuccessListener {
-            val code = codeW.text.toString()
-            val pin = pinw.text.toString()
-            val montant2 = it.child("mandat").value
-            //  montantw.text= montant2.toString()
-            montant = montant2.toString()
-            val code2 = it.child("codeOperateur").value
-            codeOperateur = code2.toString()
-            val pays1 = it.child("pays").value
-            pays = pays1.toString()
+        buttonW.setOnClickListener {
+            var database = FirebaseDatabase.getInstance().getReference("Users")
+            database.child("amir").get().addOnSuccessListener {
+                val code = codeW.text.toString()
+                val pin = pinw.text.toString()
+                val montant2 = it.child("mandat").value
+                //  montantw.text= montant2.toString()
+                montant = montant2.toString()
+                val code2 = it.child("codeOperateur").value
+                codeOperateur = code2.toString()
+                val pays1 = it.child("pays").value
+                pays = pays1.toString()
+// put data to confirmation_mandat
 
-            val intent = Intent(this, Confirmation_Mandat::class.java)
-            intent.putExtra("codeOperateur", codeOperateur)
-            intent.putExtra("pays", pays)
-            intent.putExtra("montant", montant)
-            intent.putExtra("codeW",code)
-            intent.putExtra("pinW",pin)
-            startActivity(intent)
 
-            val montant = montantw.text.toString()
-            if (code.isNotEmpty()) {
-                val codeOp = ""
-                readData(code, pin, montant,codeOp)
+//            myIntent = Intent(this, Confirmation_Mandat::class.java)
+//            intent.putExtra("codeOperateur", codeOperateur)
+//            intent.putExtra("pays", pays)
+//            intent.putExtra("montant", montant)
+//            intent.putExtra("codeW",code)
+//            intent.putExtra("pinW",pin)
+//            startActivity(intent)
+//********************************
+             val codeOp = ""
+               myIntent = Intent(this, Confirmation_Mandat::class.java)
+               myIntent.putExtra("codeOperateur", codeOperateur)
+                myIntent.putExtra("pays", pays)
+           myIntent.putExtra("montant", montant)
+               myIntent.putExtra("codeW", code)
+               myIntent.putExtra("pinW", pin)
+                // startActivity(myIntent)
+                val montant = montantw.text.toString()
+                if (!(code.isNotEmpty() && pin.isNotEmpty())) {
+                    Toast.makeText(this, "verifier votre données", Toast.LENGTH_SHORT).show()
+
+                } else {
+                    readData(code,pin,montant,codeOp)
+
+
+                }
+
+
 
 
             }
-         }
+
         }
-      /*  buttonW.setOnClickListener {
-/**/
 
-            /**/
-            val code = codeW.text.toString()
-            val pin = pinw.text.toString()
-
-            val intent = Intent(this,Confirmation_Mandat::class.java)
-            intent.putExtra("codeW",code)
-            intent.putExtra("pinW",pin)
-
-            startActivity(intent)
-
-            val montant = montantw.text.toString()
-            if (code.isNotEmpty()) {
-                val codeOp = ""
-                readData(code, pin, montant,codeOp)
-
-
-            }
-
-
-        }*/
     }
 
 
-
-
-
-
-    private fun readData(code: String, pin: String, montant: String,codeOp: String) {
+    private fun readData(code: String, pin: String, montant: String, codeOp: String)  {
         var database = FirebaseDatabase.getInstance().getReference("Users")
         database.child(code).get().addOnSuccessListener {
             val radioButton = findViewById<RadioButton>(R.id.radioButton) as RadioButton
-
-          val code_Operateur = codeOp
+            val code_Operateur = codeOp
             val code2 = it.child("codeW").value
             val pin2 = it.child("pinW").value
-            if (code == code2 && pin == pin2) {
-                if (radioButton.isChecked == false) {
-
-                    Toast.makeText(this, "checked w ija se3a", Toast.LENGTH_SHORT).show()
-                }
+            if (code == code2  && pin == pin2) {
                 Toast.makeText(this, "verified successfuly", Toast.LENGTH_SHORT).show()
+                startActivity(myIntent)
+
+
             } else {
                 Toast.makeText(this, " failed", Toast.LENGTH_SHORT).show()
             }
         }
 
     }
+    }
 
 
-
-
-}
 
 
 
